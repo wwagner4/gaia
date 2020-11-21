ede0532ac52062027613d7815924dbf39b50392e
wwagner4 <wwagner4@gmail.com>
2020-11-21 19:51:18 +0100

    more refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,105 +1,137 @@
 import java.nio.file.{Files, Path}
 
+
 import scala.io._
 import scala.util.Random
 
 
-trait Shapable {
-  def toShape: String
-}
-
-case class Position(x: Double, y: Double, z: Double) {
-  def display = s"$x, $y, $z"
-}
-
-case class Color(r: Double, g: Double, b: Double) {
-  def display = s"$r $g $b"
-}
-
-object Color {
-
-  def random: Color = {
-    val r = Random.nextDouble()
-    val g = Random.nextDouble()
-    val b = Random.nextDouble()
-    Color(r, g, b)
-  }
-
-  def red = Color(1, 0, 0)
-
-  def yellow = Color(1, 1, 0)
-
-  def orange = Color(1, 0.5, 0)
-
-  def green = Color(0, 1, 0)
-
-  def blue = Color(0, 0, 1)
-
-  def darkBlue = Color(0.1, 0.1, 0.2)
-
-  def white = Color(0, 0, 0)
-
-  def black = Color(1, 1, 1)
-
-}
-
-
-case class Cylinder(pos: Position, color: Color) extends Shapable {
-  def toShape = {
-    val c = Color.random
-    s"""
-       |<Transform translation='${pos.x}, ${pos.y}, ${pos.z}'>
-       |   <Shape>
-       |     <Cylinder radius='0.01'/>
-       |     <Appearance>
-       |       <Material diffuseColor='${c.display}'/>
-       |     </Appearance>
-       |   </Shape>
-       |</Transform>
-       |""".stripMargin
-  }
-}
-
-case class Line(pos: Position, startColor: Color, endColor: Color) extends Shapable {
-  def toShape = {
-    val r = Random.nextDouble()
-    val g = Random.nextDouble()
-    val b = Random.nextDouble()
-    s"""
-       |<Transform translation='${pos.display}'>
-       |   <Shape>
-       |   <IndexedLineSet colorIndex='0 1 -1' coordIndex='0 1 -1'>
-       |      <Color color='${startColor.display} ${endColor.display}'/>
-       |      <Coordinate point='0 0 0  1 0 0'/>
-       |   </IndexedLineSet>
-       |   </Shape>
-       |</Transform>
-       |""".stripMargin
-  }
-}
-
-
 object Main {
 
-  val backColor = Color.darkBlue
 
-  def cylCyl(xoff: Double, yoff: Double) = (0 to 500)
-    .map(i => i / 36.0)
-    .map { t =>
-      val pos = Position(math.sin(t) + xoff, t * 0.1, math.cos(t) + yoff)
-      Line(pos, Color.yellow, backColor)
-    }
+  def main(args: Array[String]): Unit = {
+    val backColor = Xml.Color.darkBlue
+    val id = "006"
+    val outfileName = s"gaia_$id.x3d"
+    val outfile = Util.outpath.resolve(outfileName)
+    val shapables = Util.randomDistribute01(Util.experimental01(backColor) _)
+    val xml = Xml.createXml(shapables, outfileName, backColor)
+    Util.writeString(outfile, xml)
+  }
+}
 
-  def ranOff: Double = {
-    (Random.nextInt(300) / 300.0 - 0.5) * 10
+object Util {
+
+  def ranOff(factor: Double): Double = (Random.nextDouble() - 0.5) * factor
+
+  lazy val outpath: Path = {
+    val outdir = System.getenv("OUTDIR")
+    if outdir == null
+      throw IllegalArgumentException("Environment variable OUTDIR must be defined")
+    val result = Path.of(outdir)
+    if !Files.exists(result)
+      throw IllegalArgumentException(s"${result} must exist")
+    result
   }
 
-  val shapables = (0 to 4)
-    .flatMap { _ => cylCyl(ranOff, ranOff) }
+  def experimental01(backColor: Xml.Color)(xoff: Double, yoff: Double, zoff: Double): Seq[Xml.Shapable] = {
+    (0 to 200)
+      .map(i => i / 50.0)
+      .map { t =>
+        val pos = Xml.Position(math.sin(t) + xoff, math.cos(t) + yoff, zoff)
+        Xml.Line(pos, Xml.Color.random, backColor)
+      }
+  }
 
-  val disp = shapables.map(x => x.toShape).mkString("\n")
 
-  def createXml(title: String): String = {
+  def writeString(outfile: Path, string: String): Unit = {
+    Files.writeString(outfile, string)
+    println(s"wrote x3d to $outfile")
+  }
+
+
+  def randomDistribute01(shape: (Double, Double, Double) => Seq[Xml.Shapable]): Seq[Xml.Shapable] = {
+    (0 to 4)
+      .flatMap { _ => shape(Util.ranOff(3), Util.ranOff(4), 0) }
+  }
+}
+
+object Xml {
+
+  case class Position(x: Double, y: Double, z: Double) {
+    def display = s"$x, $y, $z"
+  }
+
+  case class Color(r: Double, g: Double, b: Double) {
+    def display = s"$r $g $b"
+  }
+
+  object Color {
+
+    def random: Color = {
+      val r = Random.nextDouble()
+      val g = Random.nextDouble()
+      val b = Random.nextDouble()
+      Color(r, g, b)
+    }
+
+    def red = Color(1, 0, 0)
+
+    def yellow = Color(1, 1, 0)
+
+    def orange = Color(1, 0.5, 0)
+
+    def green = Color(0, 1, 0)
+
+    def blue = Color(0, 0, 1)
+
+    def darkBlue = Color(0.1, 0.1, 0.2)
+
+    def white = Color(0, 0, 0)
+
+    def black = Color(1, 1, 1)
+
+  }
+
+  trait Shapable {
+    def toShape: String
+  }
+
+  case class Cylinder(pos: Position, color: Color) extends Shapable {
+    def toShape = {
+      s"""
+         |<Transform translation='${pos.display}'>
+         |   <Shape>
+         |     <Cylinder radius='0.01'/>
+         |     <Appearance>
+         |       <Material diffuseColor='${color.display}'/>
+         |     </Appearance>
+         |   </Shape>
+         |</Transform>
+         |""".stripMargin
+    }
+  }
+
+  case class Line(pos: Position, startColor: Color, endColor: Color) extends Shapable {
+    def toShape = {
+      val r = Random.nextDouble()
+      val g = Random.nextDouble()
+      val b = Random.nextDouble()
+      s"""
+         |<Transform translation='${pos.display}'>
+         |   <Shape>
+         |   <IndexedLineSet colorIndex='0 1 -1' coordIndex='0 1 -1'>
+         |      <Color color='${startColor.display} ${endColor.display}'/>
+         |      <Coordinate point='0 0 0  1 0 0'/>
+         |   </IndexedLineSet>
+         |   </Shape>
+         |</Transform>
+         |""".stripMargin
+    }
+  }
+
+  def createXml(shapables: Seq[Shapable], title: String, backColor: Color): String = {
+
+    val theDisp = shapables.map(_.toShape).mkString("\n")
     s"""<?xml version="1.0" encoding="UTF-8"?>
        |<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 3.3//EN" "https://www.web3d.org/specifications/x3d-3.3.dtd">
        |<X3D profile='Interchange' version='3.3' xmlns:xsd='http://www.w3.org/2001/XMLSchema-instance' xsd:noNamespaceSchemaLocation='https://www.web3d.org/specifications/x3d-3.3.xsd'>
@@ -118,31 +150,11 @@ object Main {
        |    <meta content='../license.html' name='license'/>
        |  </head>
        |  <Scene>
-       |    <WorldInfo title='Cylinder.x3d'/>
+       |    <WorldInfo title='$title'/>
        |    <Background skyColor='${backColor.display}'/>
-       |    $disp
+       |    $theDisp
        |  </Scene>
        |</X3D>""".stripMargin
-  }
-
-  def writeXml(outfile: Path, xml: String): Unit = {
-    Files.writeString(outfile, xml)
-    println(s"wrote x3d to $outfile")
-  }
-
-  def main(args: Array[String]): Unit = {
-    val id = "005"
-
-
-    val outdir = System.getenv("OUTDIR")
-    if outdir == null
-      throw IllegalArgumentException("Environment variable OUTDIR must be defined")
-    val outpath = Path.of(outdir)
-    val outfileName = s"gaia_$id.x3d"
-    val outfile = outpath.resolve(outfileName)
-    val xml = createXml(outfileName)
-    writeXml(outfile, xml)
-
   }
 
 
