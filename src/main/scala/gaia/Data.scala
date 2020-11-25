28d39bc962daaef96432d4bfa808bad7deede64e
wwagner4 <wwagner4@gmail.com>
2020-11-25 21:40:21 +0100

    metadata improvement

--- src/main/scala/gaia/Data.scala
+++ src/main/scala/gaia/Data.scala
@@ -10,14 +10,17 @@ case class FileNameMd5(
 object Data {
   
   def readMeta(): Unit = {
-    readFilnameMd5.foreach(o => println(o))
+    readFilnameMd5
+      .foreach(println(_))
   }
   
   def readFilnameMd5: Iterator[FileNameMd5] = {
     val is = this.getClass.getClassLoader.getResourceAsStream("MD5SUM.txt")
     scala.io.Source.fromInputStream(is)
       .getLines()
+      .take(10)
       .map(line => line.split("\\s+"))
-      .map(a => FileNameMd5(a(0).trim, a(1).trim)) 
+      .map(a => FileNameMd5(a(0).trim, a(1).trim))
+      .filter(_.fname.startsWith("GaiaSource"))
   }
 }
