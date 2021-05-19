c58bfb3c5777c138c51812b07b1f412178c4a98c
wwagner4 <wwagner4@gmail.com>
2021-05-19 19:07:15 +0200

    create first diagram

--- src/main/scala/gaia/Tryout.scala
+++ src/main/scala/gaia/Tryout.scala
@@ -33,7 +33,7 @@ object Tryout {
     val outDir = dir.resolve("tryout-dia")
     if Files.notExists(outDir) then Files.createDirectories(outDir)
 
-    Util.runWithTmpdir{ tmpDir =>
+    Util.runWithTmpdir { tmpDir =>
       implicit val creator: VizCreator[Viz.XY] =
         VizCreators.gnuplot(
           scriptDir = tmpDir.toFile,
@@ -42,79 +42,19 @@ object Tryout {
 
       val yr = Some(Viz.Range(Some(0), Some(5)))
 
+      val data: Seq[Viz.XY] = DataUtil.starsEqualPerSector(workPath, 10, 10, 1, 2, 0.2)
+        .flatten
+        .map(s => Viz.XY(s.pos.x, s.pos.y))
+
       val multiDiagram = Viz.MultiDiagram[Viz.XY](
-        "vizMultidiagramTryout01",
-        2,
+        id = "vizMultidiagramTryout01",
+        columns = 1,
         title = Some("Multidiagram 01"),
         diagrams = Seq(
           Viz.Diagram[Viz.XY](
             "a",
             "Diagram A",
-            yRange = yr,
-            dataRows = Seq(
-              Viz.DataRow[Viz.XY](
-                data = Seq(
-                  Viz.XY(1, 2),
-                  Viz.XY(2, 1),
-                  Viz.XY(3, 1),
-                  Viz.XY(4, 2),
-                )
-              ),
-              Viz.DataRow[Viz.XY](
-                data = Seq(
-                  Viz.XY(1, 3),
-                  Viz.XY(2, 2),
-                  Viz.XY(3, 3),
-                  Viz.XY(4, 2),
-                )
-              ),
-            )
-          ),
-          Viz.Diagram[Viz.XY](
-            "b",
-            "Diagram B",
-            yRange = yr,
-            dataRows = Seq(
-              Viz.DataRow[Viz.XY](
-                data = Seq(
-                  Viz.XY(1, 1),
-                  Viz.XY(2, 1.1),
-                  Viz.XY(3, 1.2),
-                  Viz.XY(4, 1.4),
-                )
-              ),
-              Viz.DataRow[Viz.XY](
-                data = Seq(
-                  Viz.XY(1, 3),
-                  Viz.XY(2, 1),
-                  Viz.XY(3, 1),
-                  Viz.XY(4, 2),
-                )
-              ),
-            )
-          ),
-          Viz.Diagram[Viz.XY](
-            "c",
-            "Diagram C",
-            yRange = yr,
-            dataRows = Seq(
-              Viz.DataRow[Viz.XY](
-                data = Seq(
-                  Viz.XY(1, 1),
-                  Viz.XY(2, 1.1),
-                  Viz.XY(3, 1.4),
-                  Viz.XY(4, 1.8),
-                )
-              ),
-              Viz.DataRow[Viz.XY](
-                data = Seq(
-                  Viz.XY(1, 2),
-                  Viz.XY(2, 3),
-                  Viz.XY(3, 2),
-                  Viz.XY(4, 2.1),
-                )
-              ),
-            )
+            dataRows = Seq(Viz.DataRow[Viz.XY](data = data, style = Viz.Style_POINTS))
           ),
         )
       )
